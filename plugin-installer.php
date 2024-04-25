ccaa141bbf6f4b247c2d4bdf0e760dbc97ae8f78
TijnKlabbers <Tijnklabbers@gmail.com>
2024-04-25 12:09:23 +0200

    change code

--- plugin-installer.php
+++ plugin-installer.php
@@ -5,32 +5,22 @@ class AdminMessages {
     public function __construct() {
         $this->required_plugins = [
             [
-                'path' => 'advanced-custom-fields/acf.php', // Path for ACF.
+                'path' => 'advanced-custom-fields/acf.php',
                 'name' => 'Advanced Custom Fields',
                 'url'  => 'https://wordpress.org/plugins/advanced-custom-fields/'
             ],
             [
-                'path' => 'sitepress-multilingual-cms/sitepress.php', // Path for WPML.
-                'name' => 'WPML Multilingual CMS',
-                'url'  => 'https://wpml.org/'
-            ],
-            [
-                'path' => 'wp-rocket/wp-rocket.php', // Path for WP Rocket.
-                'name' => 'WP Rocket',
-                'url'  => 'https://wp-rocket.me/'
-            ],
-            [
-                'path' => 'wordfence/wordfence.php', // Path for Wordfence Security.
+                'path' => 'wordfence/wordfence.php',
                 'name' => 'Wordfence Security',
                 'url'  => 'https://wordpress.org/plugins/wordfence/'
             ],
             [
-                'path' => 'custom-post-type-ui/custom-post-type-ui.php', // Path for CPT UI.
+                'path' => 'custom-post-type-ui/custom-post-type-ui.php',
                 'name' => 'Custom Post Type UI',
                 'url'  => 'https://wordpress.org/plugins/custom-post-type-ui/'
             ],
             [
-                'path' => 'elementor/elementor.php', // Path for Elementor.
+                'path' => 'elementor/elementor.php',
                 'name' => 'Elementor Plugin',
                 'url'  => 'https://wordpress.org/plugins/elementor/'
             ]
@@ -51,11 +41,24 @@ class AdminMessages {
         }
 
         if (!empty($inactive_plugins)) {
-            echo '<div id="message" class="error"><p><strong>This theme requires the following plugins:</strong></p>';
+            echo '<div id="message" class="notice-warning notice is-dismissible"><h3>This theme requires the following plugins:</h3>';
             foreach ($inactive_plugins as $plugin_name) {
                 echo '<p>' . $plugin_name . '</p>';
             }
-            echo '<p><button onclick="document.location.href=\''. admin_url('admin-post.php?action=install_all_plugins') .'\'">Install & Ativate</button></p></div>';
+            echo '<p><button onclick="document.location.href=\''. admin_url('admin-post.php?action=install_all_plugins') .'\'">Install & Activate</button></p>';
+            echo '<button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
+            ?>
+            <script type="text/javascript">
+            jQuery(document).ready(function($){
+                $('#message').on('click', '.notice-dismiss', function (event) {
+                    event.preventDefault();
+                    $('#message').fadeOut('slow', function(){
+                        $(this).remove();
+                    });
+                });
+            });
+            </script>
+            <?php
         }
     }
 
@@ -75,24 +78,19 @@ class AdminMessages {
         foreach ($this->required_plugins as $plugin) {
             $plugin_path = $plugin['path'];
             if (!is_plugin_active($plugin_path)) {
-                // Check if the plugin is already installed
                 $installed_plugins = get_plugins();
-                if (!array_key_exists($plugin_path, $installed_plugins)) {
-                    // The plugin is not installed; attempt installation.
+                if (!array_key_exists($plugin_path, installed_plugins)) {
                     $api = plugins_api('plugin_information', ['slug' => dirname($plugin_path)]);
                     if (is_wp_error($api)) {
                         wp_die($api);
                     } else {
-                        // Download and install the plugin
                         $result = $plugin_upgrader->install($api->download_link);
                         if (is_wp_error($result)) {
                             wp_die($result);
                         }
                     }
                 }
-                // Check again if the plugin is installed (including newly installed)
                 if (array_key_exists($plugin_path, get_plugins())) {
-                    // Activate the plugin if it's installed but not active
                     activate_plugin($plugin_path);
                 }
             }
@@ -104,5 +102,4 @@ class AdminMessages {
 }
 
 new AdminMessages();
-
 ?>
